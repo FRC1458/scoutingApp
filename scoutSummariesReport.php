f65c589f9152f974e799abc81c3006e6c9eb249d
Augusata <auga9190@gmail.com>
2017-03-16 20:47:38 -0700

    Revert line 13 to Original

--- scoutSummariesReport.php
+++ scoutSummariesReport.php
@@ -10,7 +10,7 @@
     if ($conn->connect_error) {
         die("Connection: " . $conn->connect_error);
     }
-    $sql = "SELECT * FROM " . $table . WHERE teamNumber in (" . ");
+    $sql = "SELECT * FROM " . $table . "WHERE teamNumber IN (". $teams .")";
     
     $result = $conn->query($sql);
    
